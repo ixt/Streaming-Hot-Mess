a715f9aaf4fdcf7408a166dcd9f24c283f2d4c03
NfN <orange@ff4500.red>
2016-11-25 11:27:43 +0000

    Cleaning to lead for making script generic and better

--- Run.sh
+++ Run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-SOURCE="https://www.youtube.com/results?q=intitle%3A%22first+vlog%22&sp=EgIIAQ%253D%253D"
+SUBJECT=$(echo "$1" | sed -e "s/ /+/g")
+FREQ=$(echo $2s)
+SOURCE="https://www.youtube.com/results?q=intitle%3A%22$SUBJECT%22&sp=EgIIAQ%253D%253D"
 COUNT=0
 while true; do
-    lynx --dump -listonly -nonumbers $SOURCE | grep watch | sort | uniq | sed -n -e 's/.*?v=//gp' > .currentvideos
+    wget -O .page "$SOURCE"
+    cat .page | sed -n 's/.*\(\"\/watch?v=\w*\"\).*/\1/p' | sort | uniq | sed -e "s/\"\/watch?v=//g" -e "s/\"//g" > .currentvideos
+    rm .page
     cd videos
     while read entry; do
         if [ ! -e $entry.mp4 ]; then
@@ -18,7 +22,7 @@ while true; do
         fi
     done < .downloadedvideos
     (( COUNT++ ))
-    sleep 60s
+    sleep $FREQ
     echo $COUNT
 done
 
