f3d63e03611b8e1ba268e6408c50bc8b8a5eed30
NfN <orange@ff4500.red>
2016-11-26 22:23:19 +0000

    cleaned up, changed download script again to only dl medium quality videos

--- Download.sh
+++ Download.sh
@@ -1,20 +1,42 @@
-#!/usr/bin/env bash
-video_url="https://www.youtube.com/watch?v=${1}"
-# out_file_name="${2:-out.video}"
-out_file_name="${1}.video"
-curl \
-  -H 'Upgrade-insecure-requests: 1' \
-  -H 'Cache-control: max-age=0' \
-  -H 'Accept-language: en-US,en;q=0.8,bn;q=0.6' \
-  -H 'Accept-encoding: gzip, deflate, sdch' \
-  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' \
-  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36' \
-  "${video_url}" \
-| gunzip \
-| egrep -o 'https%3A%2F%2F[^\.]*\.googlevideo.com%2F[^,\]*' \
-| perl -pe 's/\%(\w\w)/chr hex $1/ge' \
-| head -n1 \
-| xargs wget -O "${out_file_name}"
+#!/bin/bash
+#
+#
+# Youtube Video Downloader bash shell version
+#
+# usage: ./Download.sh video_id
+#
+# Condensed version for Streaming Hot mess by NfN Orange
+# 2016/11/26
+# Modified 2016 NfN Orange <orange@ff4500.red>
+# Copyright 2013 Jacky Shih <iluaster@gmail.com>
+#
+# Licensed under the GNU General Public License, version 2.0 (GPLv2)
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or
+# NON INFRINGEMENT.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
-#| sed -e 's/\&itag=\w*\n/\&itag=18\n/g' \
+wget "http://www.youtube.com/get_video_info?video_id=${1}" -qO- |\
+    sed -e 's/&/\n/g' | grep 'url_encoded_fmt_stream_map' | sed -e 's/%2C/,/g' -e 's/,/\n/g' > .furls
 
+if egrep -q "(video%252Fmp4%.*quality%3Dmedium(%|\n)|quality%3Dmedium%.*video%252Fmp4(%|\n))" .furls; then
+    egrep "(video%252Fmp4%.*quality%3Dmedium(%|\n)|quality%3Dmedium%.*video%252Fmp4(%|\n))" .furls |\
+        egrep 'http|sig%3D' |sed -e 's/%26/\&/g;s/&/\n/g' > .tmp5
+fi
+
+sed -e 's/\n//g;s/sig%3D/\&signature%3D/g;s/url%3D//g;s/%25/%/g;s/%25/%/g;
+s/%3A/:/g;s/%2F/\//g;s/%3F/\?/g;s/%3D/=/g;s/%26/\&/g' .tmp5 > .tmp6
+
+wget -O "${1}.mp4" -q --show-progress -i .tmp6
+
+rm -f .tmp4 .tmp5 .tmp6 .furls
--- Run.sh
+++ Run.sh
@@ -5,20 +5,20 @@ SOURCE="https://www.youtube.com/results?q=intitle%3A%22$SUBJECT%22&sp=EgIIAQ%253
 COUNT=0
 while true; do
     wget -O .page "$SOURCE"
-    cat .page | sed -n 's/.*\(\"\/watch?v=\w*\"\).*/\1/p' | sort | uniq | sed -e "s/\"\/watch?v=//g" -e "s/\"//g" > .currentvideos
+    cat .page | sort | uniq | sed -n 's/.*\(\"\/watch?v=\w*\"\).*/\1/p' | sed -e "s/\"\/watch?v=//g" -e "s/\"//g" > .currentvideos
     rm .page
     cd videos
     while read entry; do
-        if [ ! -e $entry.video ]; then
+        if [ ! -e $entry.mp4 ]; then
             ../Download.sh $entry
-            mpv $entry.video --quiet --osc=no --geometry=$(($RANDOM % 100))%:$(($RANDOM % 100))% &
+            mpv $entry.mp4 --quiet --osc=no --geometry=$(($RANDOM % 100))%:$(($RANDOM % 100))% &
         fi
     done < ../.currentvideos
     cd ..
-    ls ./videos -1 | sed -e 's/.video//g'  > .downloadedvideos
+    ls ./videos -1 | sed -e 's/.mp4//g'  > .downloadedvideos
     while read downloaded; do
         if [ ! $(grep $downloaded .currentvideos) ]; then
-            rm videos/$downloaded.video
+            rm videos/$downloaded.mp4
         fi
     done < .downloadedvideos
     (( COUNT++ ))
