eb5abb8ea2becd0caf5d397e4d21a8331b8fd2b6
NfN <orange@ff4500.red>
2016-11-27 00:57:52 +0000

    working, help added

--- Run.sh
+++ Run.sh
@@ -1,8 +1,41 @@
 #!/usr/bin/env bash
 SUBJECT=$(echo "$1" | sed -e "s/ /+/g")
-FREQ=$(echo $2s)
-SOURCE="https://www.youtube.com/results?q=intitle%3A%22$SUBJECT%22&sp=EgIIAQ%253D%253D"
 COUNT=0
+
+helpText(){
+    echo "Usage: ./Run.sh [OPTIONS]"
+    echo "e.g. ./Run.sh -s \"Presentation\" -d 120"
+    echo "  -s \"Search term\""
+    echo "  -d Duration in seconds \(Default 60s\)"
+    echo "  -h This text"
+    echo "Use this utility to view view videos on as subject as they are uploaded"
+    echo "Run.sh written by NfN Orange 2016, CC0"
+    echo "Download Script written by Jacky Shih \<iluaster@gmail.com\> 2013 under GPLv2"
+    echo "and futhur edited in 2016 by NfN Orange"
+}
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -s) shift
+            SEARCHTERM="$(echo ${1} | sed -e "s/ /+/g")"
+            ;;
+        -d) shift
+            DURATION="${1}"
+            ;;
+        -h) shift
+            helpText
+            ;;
+         *)
+            echo "Unknown ${1}"
+            helpText
+            exit 0
+            ;;
+    esac
+    shift
+done
+
+SOURCE="https://www.youtube.com/results?q=intitle%3A%22$SEARCHTERM%22&sp=EgIIAQ%253D%253D"
+
 while true; do
     wget -O .page "$SOURCE"
     cat .page | sort | uniq | sed -n 's/.*\(\"\/watch?v=\w*\"\).*/\1/p' | sed -e "s/\"\/watch?v=//g" -e "s/\"//g" > .currentvideos
@@ -11,7 +44,7 @@ while true; do
     while read entry; do
         if [ ! -e $entry.mp4 ]; then
             ../Download.sh $entry
-            mpv $entry.mp4 --quiet --osc=no --geometry=$(($RANDOM % 100))%:$(($RANDOM % 100))% &
+            mpv $entry.mp4 --quiet
         fi
     done < ../.currentvideos
     cd ..
@@ -22,7 +55,7 @@ while true; do
         fi
     done < .downloadedvideos
     (( COUNT++ ))
-    sleep $FREQ
-    echo $COUNT
+    sleep ${DURATION:-60}s
+    echo "loop $COUNT"
 done
 
